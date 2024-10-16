15a54c11e5e4e0bab43d9eb6b94fa92ef151aa86
EmiIsaac4545 <149457956+EmiIsaac4545@users.noreply.github.com>
2024-10-16 07:52:30 -0600

    Implemented changes to the file

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,21 +1,16 @@
-
-lyst = [4, 3, 5, 8, -2, 10, 67]
-
-def insertion_Sort (lyst):
+def insertion_sort (lyst):
 
     i = 1
 
     while i < len(lyst):
 
         target = lyst[i]
-
         j = i - 1
 
         while j >= 0:
             if target < lyst[j]:
 
                 lyst[ j + 1] = lyst [j]
-
                 j -=1
 
             else : 
@@ -24,7 +19,36 @@ def insertion_Sort (lyst):
         lyst[j + 1] = target
         i += 1
 
-m
+    return lyst
+
+def get_valid_input():
+
+    while True:
+
+        try:
+            # Receive input from the user, split by spaces, and convert to a list of integers
+            user_input = input ("Enter numbers separated by spaces ( All numbers must be non-negative): ")
+            lyst = list(map(int, user_input.split()))
+
+            # Check if any number is negative
+            if any (num < 0 for num in lyst):
+                print ("All numbers must be non-negative. Please try again.")
+
+            else:
+                return lyst
+
+        except ValueError:
+            print("Invalid input.Plese enter valid intgers")
+
+# get valid input from the user
+lyst = get_valid_input()
+
+# Call the insertion_sort function
+result = insertion_sort(lyst)
+
+# Print result
+print ("The sorting list is:", result)
+
 
 
 
